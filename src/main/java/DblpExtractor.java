e97da5d81f6b7327bd915e32e77155df0915f1fc
Alejandro Lozano <lozanojalex@gmail.com>
2020-11-13 13:42:51 +0100

    First steps into the DBLP JSON file extraction

--- src/main/java/DblpExtractor.java
+++ src/main/java/DblpExtractor.java
@@ -11,33 +11,52 @@ import java.io.IOException;
  * See reference: https://howtodoinjava.com/java/library/json-simple-read-write-json-examples/
  */
 public class DblpExtractor {
-    public DblpExtractor() {
-
-    }
 
     public static void extractDataIntoWarehouse() {
-        //JSON parser object to parse read file
-        JSONParser jsonParser = new JSONParser();
-
         try (FileReader fileReader = new FileReader("src/main/resources/dblp/dblp-solo-article-1.json")) {
             //Read JSON file
+            JSONParser jsonParser = new JSONParser();
             JSONObject entireJsonFile = (JSONObject) jsonParser.parse(fileReader);
             JSONObject jsonObjectContainer = (JSONObject) entireJsonFile.get("dblp");
             JSONArray articles = (JSONArray) jsonObjectContainer.get("article");
 
             articles.forEach(article -> parseJsonObject((JSONObject) article));
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private static void parseJsonObject(JSONObject jsonObject) {
-        Long year = (Long) jsonObject.get("year");
-        System.out.println(year);
+        try {
+            String title = (String) jsonObject.get("title");
+            Long year = (Long) jsonObject.get("year");
+            // TODO: Check whether "ee" exists to avoid a NullReference on retrieving "content".
+            // String url = (String) ((JSONObject) jsonObject.get("ee")).get("content");
+
+            // TODO: Check whether the "pages" is a Long, containing the total number of pages, or a String,
+            //  having the initial-final pages.
+            String pages = (String) jsonObject.get("pages");
+
+            if (pages != null) {
+                String initialPages = extractInitialPages(pages);
+                System.out.println(initialPages);
+
+                String finalPages = extractFinalPages(pages);
+            }
+
+        } catch (ClassCastException e) {
+            System.err.println("An error has ocurred while retrieving the JSONObject " + jsonObject.toString());
+            e.printStackTrace();
+        }
+    }
+
+    private static String extractInitialPages(String pages) {
+        return pages.substring(0, pages.indexOf("-"));
+    }
+
+    private static String extractFinalPages(String pages) {
+        // The '+ 1' is because String.substring(int startIndex) includes the char at startIndex.
+        return pages.substring(pages.indexOf("-") + 1);
     }
 }
