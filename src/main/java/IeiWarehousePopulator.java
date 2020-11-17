4f7bac9ba0ddec9165cd42b56d6bd46fc321dcb4
Alejandro Lozano <lozanojalex@gmail.com>
2020-11-17 21:38:20 +0100

    Renamed program name

--- src/main/java/IeiWarehousePopulator.java
+++ src/main/java/IeiWarehousePopulator.java
@@ -1,4 +1,4 @@
-public class MainClassTest {
+public class IeiWarehousePopulator {
     public static void main(String[] args) {
         //MySQLConnection.performUpdate("INSERT INTO persona (nombre, apellidos) VALUES (\"JAVIER\", \"Vicente\")");
         DblpExtractor.extractDataIntoWarehouse();
