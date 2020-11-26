0ba03effdc4c57d50dcb779d6e7f1be61cc14e2f
RICARDO SOLER BACHS <risobac@upv.edu.es>
2020-11-26 11:00:57 +0100

    Add Book persistence to the MySQL Database

--- src/main/java/ieiWarehousePopulator/domain/Book.java
+++ src/main/java/ieiWarehousePopulator/domain/Book.java
@@ -1,5 +1,6 @@
 package ieiWarehousePopulator.domain;
 
+import ieiWarehousePopulator.persistence.MySQLConnection;
 import ieiWarehousePopulator.persistence.Persistable;
 
 public class Book extends Publication implements Persistable {
@@ -12,6 +13,32 @@ public class Book extends Publication implements Persistable {
 
     @Override
     public void persist() {
+        Integer retrievedPublicationId = super.retrievePublicationDatabaseId();
+
+        if (!doesBookAlreadyExistInDatabase(retrievedPublicationId)) {
+            super.insertNewPublicationIntoDatabase();
+            this.insertNewBookIntoDatabase();
+
+            retrievedPublicationId = super.retrievePublicationDatabaseId();
+            Person.persistAuthors(this.getAuthors(), retrievedPublicationId);
+        } else {
+            // TODO: Notify that the book already exists in database.
+        }
+    }
+
+    private boolean doesBookAlreadyExistInDatabase(Integer retrievedId) {
+        return retrievedId != null;
+    }
+
+    private void insertNewBookIntoDatabase() {
+        Integer retrievedPublicationId = super.retrievePublicationDatabaseId();
+
+        String addBookSqlUpdate =
+                "INSERT INTO libro (publicacion_id, editorial) " +
+                        "VALUES (" + retrievedPublicationId + ", " +
+                        "\"" + this.getEditorial() + "\");";
+
+        MySQLConnection.performUpdate(addBookSqlUpdate);
     }
 
     public String getEditorial() {
