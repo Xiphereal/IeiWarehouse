f00cc88cfe3fbe04c6a09588e311137fa1ee8e14
RICARDO SOLER BACHS <risobac@upv.edu.es>
2020-11-26 12:19:55 +0100

    Add Congress Communication persistence to the MySQL Database

--- src/main/java/ieiWarehousePopulator/domain/CongressCommunication.java
+++ src/main/java/ieiWarehousePopulator/domain/CongressCommunication.java
@@ -1,5 +1,6 @@
 package ieiWarehousePopulator.domain;
 
+import ieiWarehousePopulator.persistence.MySQLConnection;
 import ieiWarehousePopulator.persistence.Persistable;
 
 public class CongressCommunication extends Publication implements Persistable {
@@ -27,6 +28,36 @@ public class CongressCommunication extends Publication implements Persistable {
 
     @Override
     public void persist() {
+        Integer retrievedPublicationId = super.retrievePublicationDatabaseId();
+
+        if (!doesCongressCommunicationAlreadyExistInDatabase(retrievedPublicationId)) {
+            super.insertNewPublicationIntoDatabase();
+            this.insertNewCongressCommunicationIntoDatabase();
+
+            retrievedPublicationId = super.retrievePublicationDatabaseId();
+            Person.persistAuthors(this.getAuthors(), retrievedPublicationId);
+        } else {
+            // TODO: Notify that the Congress Communication already exists in database.
+        }
+    }
+
+    private boolean doesCongressCommunicationAlreadyExistInDatabase(Integer retrievedId) {
+        return retrievedId != null;
+    }
+
+    private void insertNewCongressCommunicationIntoDatabase() {
+        Integer retrievedPublicationId = super.retrievePublicationDatabaseId();
+
+        String addCongressCommunicationSqlUpdate =
+                "INSERT INTO comunicacioncongreso (publicacion_id, congreso, edicion, lugar, pagina_inicio, pagina_fin) " +
+                        "VALUES (" + retrievedPublicationId + ", " +
+                        "\"" + this.getCongress() + "\"" + ", " +
+                        "\"" + this.getEdition() + "\"" + ", " +
+                        "\"" + this.getPlace() + "\"" + ", " +
+                        this.getInitialPage() + ", " +
+                        this.getFinalPage() + ");";
+
+        MySQLConnection.performUpdate(addCongressCommunicationSqlUpdate);
     }
 
     public String getCongress() {
