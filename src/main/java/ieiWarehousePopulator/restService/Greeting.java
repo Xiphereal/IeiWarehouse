45b7134017218be079ea432f0ed991d36a694b52
Alejandro Lozano <lozanojalex@gmail.com>
2020-12-21 22:01:01 +0100

    Add the test resource representation class

---
+++ src/main/java/ieiWarehousePopulator/restService/Greeting.java
@@ -0,0 +1,22 @@
+package ieiWarehousePopulator.restService;
+
+/**
+ * This is a test resource representation class. Must be removed upon tutorial completion.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
