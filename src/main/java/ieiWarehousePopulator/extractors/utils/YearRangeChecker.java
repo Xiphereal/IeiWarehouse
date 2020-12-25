e551f186a5a161dca1db9bb0f08a6582f54118a3
Alejandro Lozano <lozanojalex@gmail>
2020-12-25 02:13:44 +0100

    Over-approximate year range check if there's no given year

--- src/main/java/ieiWarehousePopulator/extractors/utils/YearRangeChecker.java
+++ src/main/java/ieiWarehousePopulator/extractors/utils/YearRangeChecker.java
@@ -2,6 +2,11 @@ package ieiWarehousePopulator.extractors.utils;
 
 public class YearRangeChecker {
     public static boolean isGivenYearBetweenRange(Long givenYear, Long startYear, Long endYear) {
+
+        // If there is no given year, the result is over-approximated.
+        if (givenYear == null)
+            return true;
+
         return startYear <= givenYear && givenYear <= endYear;
     }
 }
