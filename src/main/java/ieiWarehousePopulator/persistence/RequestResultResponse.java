3eaf56d026dbd6136ada8fb050ae0ec21f4dc495
Xiphereal <lozanojalex@gmail.com>
2020-12-26 23:56:15 +0100

    Add RequestResultResponse

---
+++ src/main/java/ieiWarehousePopulator/persistence/RequestResultResponse.java
@@ -0,0 +1,43 @@
+package ieiWarehousePopulator.persistence;
+
+import ieiWarehousePopulator.domain.Article;
+import ieiWarehousePopulator.domain.Book;
+import ieiWarehousePopulator.domain.CongressCommunication;
+
+import java.util.List;
+
+public class RequestResultResponse {
+    private List<Article> articles;
+    private List<Book> books;
+    private List<CongressCommunication> congressCommunications;
+
+    public RequestResultResponse(List<Article> articles, List<Book> books, List<CongressCommunication> congressCommunications) {
+        this.articles = articles;
+        this.books = books;
+        this.congressCommunications = congressCommunications;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public List<CongressCommunication> getCongressCommunications() {
+        return congressCommunications;
+    }
+
+    public void setCongressCommunications(List<CongressCommunication> congressCommunications) {
+        this.congressCommunications = congressCommunications;
+    }
+}
