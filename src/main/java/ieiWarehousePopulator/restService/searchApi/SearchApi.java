451e400e6912a197c6896cf3944630d92d7ee970
Xiphereal <lozanojalex@gmail.com>
2020-12-27 00:09:31 +0100

    Adapt SearchApi for allowing multiple RequestResponses

--- src/main/java/ieiWarehousePopulator/restService/searchApi/SearchApi.java
+++ src/main/java/ieiWarehousePopulator/restService/searchApi/SearchApi.java
@@ -1,14 +1,17 @@
 package ieiWarehousePopulator.restService.searchApi;
 
+import ieiWarehousePopulator.domain.Article;
+import ieiWarehousePopulator.domain.Book;
+import ieiWarehousePopulator.domain.CongressCommunication;
 import ieiWarehousePopulator.extractors.utils.YearRange;
-import ieiWarehousePopulator.persistence.dataAccessObjects.ArticleDAO;
-import ieiWarehousePopulator.persistence.dataAccessObjects.BookDAO;
-import ieiWarehousePopulator.persistence.dataAccessObjects.CongressCommunicationDAO;
-import ieiWarehousePopulator.restService.RequestStatusResponse;
+import ieiWarehousePopulator.restService.requestResponses.RequestResponse;
+import ieiWarehousePopulator.restService.requestResponses.RequestResultResponse;
+import ieiWarehousePopulator.restService.requestResponses.RequestStatusResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -28,12 +31,12 @@ public class SearchApi {
      * "http://localhost:8080/extractData?startYear=2015&endYear=2018&extractFromDBLP=false&extractFromGoogleScholar=false"
      */
     @GetMapping("/getData")
-    public RequestStatusResponse getData(@RequestParam(value = "startYear", defaultValue = "1000") String startYear,
-                                         @RequestParam(value = "endYear", defaultValue = "2999") String endYear,
-                                         @RequestParam(value = "author", defaultValue = "") String author,
-                                         @RequestParam(value = "searchArticles", defaultValue = "true") boolean searchArticles,
-                                         @RequestParam(value = "searchCongressCommunications", defaultValue = "true") boolean searchCongressCommunications,
-                                         @RequestParam(value = "searchBooks", defaultValue = "true") boolean searchBooks) {
+    public RequestResponse getData(@RequestParam(value = "startYear", defaultValue = "1000") String startYear,
+                                   @RequestParam(value = "endYear", defaultValue = "2999") String endYear,
+                                   @RequestParam(value = "author", defaultValue = "") String author,
+                                   @RequestParam(value = "searchArticles", defaultValue = "true") boolean searchArticles,
+                                   @RequestParam(value = "searchCongressCommunications", defaultValue = "true") boolean searchCongressCommunications,
+                                   @RequestParam(value = "searchBooks", defaultValue = "true") boolean searchBooks) {
 
         boolean isYearRangeValid = YearRange.isRangeValid(startYear, endYear);
 
@@ -43,47 +46,24 @@ public class SearchApi {
 
         YearRange yearRange = new YearRange(Long.valueOf(startYear), Long.valueOf(endYear));
 
-        String responseContents = getResponseContentsFromWarehouse(yearRange,
+        return getResponseContentsFromWarehouse(yearRange,
                 author,
                 searchArticles,
                 searchBooks,
                 searchCongressCommunications);
-
-        return new RequestStatusResponse(requestId.incrementAndGet(), responseContents);
     }
 
-    private String getResponseContentsFromWarehouse(YearRange yearRange,
-                                                    String author,
-                                                    boolean searchArticles,
-                                                    boolean searchBooks,
-                                                    boolean searchCongressCommunications) {
-        StringBuilder responseContents = new StringBuilder();
+    private RequestResultResponse getResponseContentsFromWarehouse(YearRange yearRange,
+                                                                   String author,
+                                                                   boolean searchArticles,
+                                                                   boolean searchBooks,
+                                                                   boolean searchCongressCommunications) {
+        List<Article> retrievedArticles = new ArrayList<>();
+        List<Book> retrievedBooks = new ArrayList<>();
+        List<CongressCommunication> retrievedCongressCommunications = new ArrayList<>();
 
-        if (searchArticles) {
-            List<ArticleDAO> articleDAOs = ArticleDAO.getArticleDAOs();
+        // TODO: Retrieve the different publications from the persistence layer.
 
-            for (ArticleDAO articleDAO : articleDAOs) {
-                // TODO: Append each article to the response contents.
-            }
-        }
-
-        if (searchBooks) {
-            List<BookDAO> bookDAOs = BookDAO.getBookDAOs();
-
-            for (BookDAO bookDAO : bookDAOs) {
-                // TODO: Append each book to the response contents.
-            }
-        }
-
-        if (searchCongressCommunications) {
-            List<CongressCommunicationDAO> congressCommunicationDAOs =
-                    CongressCommunicationDAO.getCongressCommunicationDAOs();
-
-            for (CongressCommunicationDAO congressCommunicationDAO : congressCommunicationDAOs) {
-                // TODO: Append each communication congress to the response contents.
-            }
-        }
-
-        return responseContents.toString();
+        return new RequestResultResponse(retrievedArticles, retrievedBooks, retrievedCongressCommunications);
     }
 }
