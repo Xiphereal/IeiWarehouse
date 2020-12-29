1734bc3215daddac3e09f96ebf8efb03cb1cda7b
jadomen <jadomen@inf.upv.es>
2020-12-29 17:48:38 +0100

    BookDAO retrieveBooks in order to retrieve books from the db

--- src/main/java/ieiWarehousePopulator/persistence/dataAccessObjects/BookDAO.java
+++ src/main/java/ieiWarehousePopulator/persistence/dataAccessObjects/BookDAO.java
@@ -9,18 +9,18 @@ import java.util.List;
 
 public class BookDAO extends PublicationDAO {
     public static List<Book> retrieveBooks(YearRange yearRange, String author) {
+        // TODO: Populate the result list with the filtered Articles from the Warehouse.
+        // TODO: Retrieve the list of the article's authors.
         String sqlQuery =
                 "SELECT DISTINCT titulo, anyo, url, " +
-                        "pagina_inicio, pagina_fin, " +
-                        "volumen, numero, mes, " +
-                        "nombre " +
+                        "editorial " +
                         "FROM publicacion " +
-                        "LEFT JOIN articulo ON articulo.publicacion_id=publicacion.id " +
-                        "LEFT JOIN ejemplar ON articulo.ejemplar_id=ejemplar.id " +
-                        "LEFT JOIN revista ON ejemplar.revista_id=revista.id " +
+                        "LEFT JOIN articulo ON libro.publicacion_id=publicacion.id " +
                         "WHERE anyo >= " + yearRange.getStartYear() + " " +
                         "AND anyo <= " + yearRange.getEndYear() + " " +
                         "GROUP BY titulo;";
+
+
 
         return MySQLConnection.performQueryToRetrieveBooks(sqlQuery);
     }
