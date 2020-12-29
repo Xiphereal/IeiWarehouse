03a8902fc3dcc24050d529ea7266ec03bca02ab5
Xiphereal <lozanojalex@gmail.com>
2020-12-29 02:22:17 +0100

    Add TODO about the table purge util in program entrypoint

--- src/main/java/ieiWarehousePopulator/IeiWarehousePopulator.java
+++ src/main/java/ieiWarehousePopulator/IeiWarehousePopulator.java
@@ -9,10 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IeiWarehousePopulator {
     public static void main(String[] args) {
 
+        // TODO: Remove the table purge when both load & search APIs are fully
+        //  implemented. It's just a util for dev and debugging purposes.
         DatabasePurge.purgeAllTables();
 
         SpringApplication.run(IeiWarehousePopulator.class, args);
-        
+
         // Closes the SQL connection even when the VM terminates abruptly.
         // ! Doesn't work in IDE executions.
         // See: https://stackoverflow.com/questions/3366965/is-it-is-possible-to-do-something-when-the-java-program-exits-abruptly
