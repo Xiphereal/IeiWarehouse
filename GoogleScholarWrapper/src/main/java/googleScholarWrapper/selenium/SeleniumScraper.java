72247e381ee8f74c66cb08f57b7834a1ce561dae
Xiphereal <lozanojalex@gmail.com>
2020-12-30 13:54:54 +0100

    Replace Selenium "ping" test with the first steps into the data scraping

--- GoogleScholarWrapper/src/main/java/googleScholarWrapper/selenium/SeleniumScraper.java
+++ GoogleScholarWrapper/src/main/java/googleScholarWrapper/selenium/SeleniumScraper.java
@@ -1,7 +1,7 @@
 package googleScholarWrapper.selenium;
 
+import org.jetbrains.annotations.NotNull;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,13 +9,29 @@ public class SeleniumScraper {
     private static final String PROJECT_PATH = System.getProperty("user.dir") + "/GoogleScholarWrapper/";
 
     public static void scrap() {
+        ChromeDriver chromeDriver = openChromeInstance();
+
+        openDrawerMenu(chromeDriver);
+
+        openAdvancedSearch(chromeDriver);
+    }
+
+    private static void openAdvancedSearch(ChromeDriver chromeDriver) {
+        WebElement advancedSearchMenuItem = chromeDriver.findElement(By.xpath("//*[@id=\"gs_hp_drw_adv\"]"));
+        advancedSearchMenuItem.click();
+    }
+
+    @NotNull
+    private static ChromeDriver openChromeInstance() {
         System.setProperty("webdriver.chrome.driver", PROJECT_PATH + "src/main/resources/chromedriver_ver87.exe");
         ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("https://www.google.com");
+        chromeDriver.get("https://scholar.google.com/");
 
-        WebElement searchTextBox = chromeDriver.findElement(By.name("q"));
-        searchTextBox.sendKeys("Selenium 4 works as charms! :)" + Keys.ENTER);
+        return chromeDriver;
+    }
 
-        chromeDriver.quit();
+    private static void openDrawerMenu(ChromeDriver chromeDriver) {
+        WebElement drawerMenuButton = chromeDriver.findElement(By.xpath("//*[@id=\"gs_hdr_mnu\"]"));
+        drawerMenuButton.click();
     }
 }
