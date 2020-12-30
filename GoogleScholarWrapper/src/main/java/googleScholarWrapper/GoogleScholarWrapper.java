ceb3ae122fca71bb033b57719a87fbac0b480fea
Xiphereal <lozanojalex@gmail>
2020-12-30 02:24:12 +0100

    Add basic startup test for Selenium

--- GoogleScholarWrapper/src/main/java/googleScholarWrapper/GoogleScholarWrapper.java
+++ GoogleScholarWrapper/src/main/java/googleScholarWrapper/GoogleScholarWrapper.java
@@ -1,7 +1,24 @@
 package googleScholarWrapper;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class GoogleScholarWrapper {
+    private static final String PROJECT_PATH = System.getProperty("user.dir") + "/GoogleScholarWrapper/";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.setProperty("webdriver.chrome.driver", PROJECT_PATH + "src/main/resources/chromedriver_ver87.exe");
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://www.google.com");
+
+        WebElement searchTextBox = chromeDriver.findElement(By.name("q"));
+        searchTextBox.sendKeys("Selenium 4 works as charms! :)" + Keys.ENTER);
+
+        chromeDriver.quit();
+
+        // Terminates the program normally.
+        System.exit(0);
     }
 }
