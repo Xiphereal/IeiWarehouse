bfcd480477535beb99f312a5349d6379883627a0
Xiphereal <lozanojalex@gmail.com>
2020-12-30 13:00:13 +0100

    Move Selenium related logic to SeleniumScraper

--- GoogleScholarWrapper/src/main/java/googleScholarWrapper/selenium/SeleniumScraper.java
+++ GoogleScholarWrapper/src/main/java/googleScholarWrapper/selenium/SeleniumScraper.java
@@ -1,4 +1,21 @@
 package googleScholarWrapper.selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumScraper {
+    private static final String PROJECT_PATH = System.getProperty("user.dir") + "/GoogleScholarWrapper/";
+
+    public static void scrap() {
+        System.setProperty("webdriver.chrome.driver", PROJECT_PATH + "src/main/resources/chromedriver_ver87.exe");
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://www.google.com");
+
+        WebElement searchTextBox = chromeDriver.findElement(By.name("q"));
+        searchTextBox.sendKeys("Selenium 4 works as charms! :)" + Keys.ENTER);
+
+        chromeDriver.quit();
+    }
 }
