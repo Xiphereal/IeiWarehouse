05e5f89dc5e5cfe20893ca66eafe8899a2f46be7
Xiphereal <lozanojalex@gmail.com>
2021-01-02 19:30:07 +0100

    Modify the module to use Vaadin UI

--- Frontend/src/main/java/frontend/FrontendApplication.java
+++ Frontend/src/main/java/frontend/FrontendApplication.java
@@ -1,12 +1,14 @@
 package frontend;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FrontendApplication {
+@Theme("valo")
+public class FrontendApplication extends UI {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FrontendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendApplication.class, args);
+	}
+
 }
