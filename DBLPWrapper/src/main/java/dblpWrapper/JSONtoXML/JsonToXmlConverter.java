04fb1f837530ac4196ebc7f26db79cd19b423dea
jadomen <jadomen@inf.upv.es>
2021-01-02 15:52:57 +0100

    xml to json converter

---
+++ DBLPWrapper/src/main/java/dblpWrapper/JSONtoXML/JsonToXmlConverter.java
@@ -0,0 +1,27 @@
+package dblpWrapper.JSONtoXML;
+
+import org.json.JSONObject;
+import org.json.XML;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JsonToXmlConverter {
+    private static String PATH_TO_XML = "resources/bd.xml";
+
+    public static JSONObject convert() throws IOException {
+        StringBuilder dataFromXML = new StringBuilder();
+        String line = "";
+        BufferedReader br = new BufferedReader(new FileReader(PATH_TO_XML));
+
+        while ((line = br.readLine()) != null) {
+            dataFromXML.append(line);
+        }
+
+        JSONObject jsonObject = XML.toJSONObject(dataFromXML.toString());
+        return jsonObject;
+    }
+
+}
