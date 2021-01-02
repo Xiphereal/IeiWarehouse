d988862db1df1d53fc7c48db9eecadbac3529cb3
jadomen <jadomen@inf.upv.es>
2021-01-02 17:59:57 +0100

    json to xml converter and json parser by year

--- DBLPWrapper/src/main/java/dblpWrapper/DBLPWrapper.java
+++ DBLPWrapper/src/main/java/dblpWrapper/DBLPWrapper.java
@@ -1,11 +1,37 @@
 package dblpWrapper;
 
+import dblpWrapper.JSONtoXML.JsonToXmlConverter;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class DBLPWrapper {
-    public static void main(String[] args) {
-        SpringApplication.run(DBLPWrapper.class, args);
+    public static void main(String[] args) throws IOException {
+       //System.out.println(JsonToXmlConverter.convert().toString(3));
+
+       SpringApplication.run(DBLPWrapper.class, args);
+    }
+
+    private List<JSONObject> filterByYear(int yearStart, int yearEnd) throws IOException {
+        JSONObject convertedFilie = JsonToXmlConverter.convert();
+        JSONObject globalObject = convertedFilie.getJSONObject("dblp");
+        //System.out.println(globalObject.toString(2));
+
+        JSONArray articles = globalObject.getJSONArray("article");
+        List<JSONObject> validArticles = new ArrayList<>();
+        for(int i = 0; i < articles.length(); i++) {
+            int year = articles.getJSONObject(i).getInt("year");
+            if(year >= yearStart && year <= yearEnd) {
+                validArticles.add(articles.getJSONObject(i));
+            }
+        }
+        return validArticles;
     }
 }
--- DBLPWrapper/src/main/java/dblpWrapper/JSONtoXML/JsonToXmlConverter.java
+++ DBLPWrapper/src/main/java/dblpWrapper/JSONtoXML/JsonToXmlConverter.java
@@ -9,13 +9,12 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class JsonToXmlConverter {
-    private static String PATH_TO_XML = "resources/bd.xml";
+    private static String PATH_TO_XML = "src/main/java/dblpWrapper/JSONtoXML/DBLP-SOLO_ARTICLE-1.XML";
 
     public static JSONObject convert() throws IOException {
         StringBuilder dataFromXML = new StringBuilder();
         String line = "";
         BufferedReader br = new BufferedReader(new FileReader(PATH_TO_XML));
-
         while ((line = br.readLine()) != null) {
             dataFromXML.append(line);
         }
