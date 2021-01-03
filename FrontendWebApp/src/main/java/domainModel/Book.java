0e432e28ac4b643202effed567ded4293151b37d
Xiphereal <lozanojalex@gmail.com>
2021-01-03 14:59:13 +0100

    Temporal addition of DomainModel entities

---
+++ FrontendWebApp/src/main/java/domainModel/Book.java
@@ -0,0 +1,25 @@
+package domainModel;
+
+public class Book extends Publication {
+    private String publisher;
+
+    public Book(String title, Long year, String url, String editorial) {
+        super(title, year, url);
+        this.publisher = editorial;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "editorial='" + publisher + '\'' +
+                "} " + super.toString();
+    }
+}
---
+++ FrontendWebApp/src/main/java/domainModel/Copy.java
@@ -0,0 +1,56 @@
+package domainModel;
+
+public class Copy {
+    private Integer volume;
+    private Integer number;
+    private Integer month;
+    private Magazine magazinePublishBy;
+
+    public Copy(Integer volume, Integer number, Integer month) {
+        this.volume = volume;
+        this.number = number;
+        this.month = month;
+    }
+
+    public Integer getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Integer volume) {
+        this.volume = volume;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public void setMonth(Integer month) {
+        this.month = month;
+    }
+
+    public Magazine getMagazinePublishBy() {
+        return magazinePublishBy;
+    }
+
+    public void setMagazinePublishBy(Magazine magazinePublishBy) {
+        this.magazinePublishBy = magazinePublishBy;
+    }
+
+    @Override
+    public String toString() {
+        return "Copy{" +
+                "volume=" + volume +
+                ", number=" + number +
+                ", month=" + month +
+                ", magazinePublishBy=" + magazinePublishBy +
+                '}';
+    }
+}
