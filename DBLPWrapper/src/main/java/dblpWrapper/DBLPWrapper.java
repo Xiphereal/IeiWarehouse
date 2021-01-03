c9cea8a7ee427f56ee4ca3baa4855f58a7c61991
jadomen <jadomen@inf.upv.es>
2021-01-03 12:10:56 +0100

    first functional version

--- DBLPWrapper/src/main/java/dblpWrapper/DBLPWrapper.java
+++ DBLPWrapper/src/main/java/dblpWrapper/DBLPWrapper.java
@@ -18,20 +18,4 @@ public class DBLPWrapper {
 
        SpringApplication.run(DBLPWrapper.class, args);
     }
-
-    private List<JSONObject> filterByYear(int yearStart, int yearEnd) throws IOException {
-        JSONObject convertedFilie = JsonToXmlConverter.convert();
-        JSONObject globalObject = convertedFilie.getJSONObject("dblp");
-        //System.out.println(globalObject.toString(2));
-
-        JSONArray articles = globalObject.getJSONArray("article");
-        List<JSONObject> validArticles = new ArrayList<>();
-        for(int i = 0; i < articles.length(); i++) {
-            int year = articles.getJSONObject(i).getInt("year");
-            if(year >= yearStart && year <= yearEnd) {
-                validArticles.add(articles.getJSONObject(i));
-            }
-        }
-        return validArticles;
-    }
 }
--- DBLPWrapper/src/main/java/dblpWrapper/JSONtoXML/JsonToXmlConverter.java
+++ DBLPWrapper/src/main/java/dblpWrapper/JSONtoXML/JsonToXmlConverter.java
@@ -1,12 +1,15 @@
 package dblpWrapper.JSONtoXML;
 
+import domainModel.Publication;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.XML;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonToXmlConverter {
     private static String PATH_TO_XML = "src/main/java/dblpWrapper/JSONtoXML/DBLP-SOLO_ARTICLE-1.XML";
@@ -22,4 +25,19 @@ public class JsonToXmlConverter {
         return jsonObject;
     }
 
+    public static List<JSONObject> filterByYear(int yearStart, int yearEnd) throws IOException {
+        JSONObject convertedFilie = JsonToXmlConverter.convert();
+        JSONObject globalObject = convertedFilie.getJSONObject("dblp");
+        //System.out.println(globalObject.toString(2));
+
+        JSONArray articles = globalObject.getJSONArray("article");
+        List<JSONObject> validArticles = new ArrayList<>();
+        for(int i = 0; i < articles.length(); i++) {
+            int year = articles.getJSONObject(i).getInt("year");
+            if(year >= yearStart && year <= yearEnd) {
+                validArticles.add(articles.getJSONObject(i));
+            }
+        }
+        return validArticles;
+    }
 }
