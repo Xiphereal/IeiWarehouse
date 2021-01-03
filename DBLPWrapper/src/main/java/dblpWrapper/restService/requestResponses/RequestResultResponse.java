c2cc5cbe15e069649914fb021cc5a8527daf58d2
jadomen <jadomen@inf.upv.es>
2021-01-03 11:15:46 +0100

    Modified RequestResultResponse in order to adapt to the data we will be giving

---
+++ DBLPWrapper/src/main/java/dblpWrapper/restService/requestResponses/RequestResultResponse.java
@@ -0,0 +1,23 @@
+package dblpWrapper.restService.requestResponses;
+
+import domainModel.Article;
+import domainModel.Book;
+import domainModel.CongressCommunication;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class RequestResultResponse implements RequestResponse{
+    private List<Article> articles;
+
+    public RequestResultResponse(List<Article> articles, List<Book> books, List<CongressCommunication> congressCommunications) {
+        this.articles = articles;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+    public void setArticles(List<Article> articles) {
+       this.articles = articles;
+    }
+}
