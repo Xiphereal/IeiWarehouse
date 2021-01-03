8600e6c9a0d94312a22df2366ffcc58b2f2e33cb
Xiphereal <lozanojalex@gmail.com>
2021-01-03 14:22:15 +0100

    Add extractor checkboxes options to DataExtractionView

--- FrontendWebApp/src/main/java/frontendWebApp/views/dataExtraction/DataExtractionView.java
+++ FrontendWebApp/src/main/java/frontendWebApp/views/dataExtraction/DataExtractionView.java
@@ -1,6 +1,8 @@
 package frontendWebApp.views.dataExtraction;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.CheckboxGroup;
+import com.vaadin.flow.component.checkbox.CheckboxGroupVariant;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -8,6 +10,9 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import frontendWebApp.views.main.MainView;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Route(value = "data-extraction", layout = MainView.class)
 @PageTitle("Extracción de datos")
@@ -19,12 +24,33 @@ public class DataExtractionView extends HorizontalLayout {
 
     public DataExtractionView() {
         setId("dataextraction-view");
+
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
+
         add(name, sayHello);
+
         setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        addExtractorsCheckboxOptions();
+
         sayHello.addClickListener(e -> {
             Notification.show("Hello " + name.getValue());
         });
+    }
+
+    private void addExtractorsCheckboxOptions() {
+        CheckboxGroup<String> extractorsOptions = new CheckboxGroup<>();
+        extractorsOptions.setLabel("Extractores a utilizar");
+        extractorsOptions.setItems("DBLP", "IEEE", "Google Scholar");
+
+        Set<String> values = new HashSet<>();
+        values.add("DBLP");
+        values.add("IEEE");
+        values.add("Google Scholar");
+        extractorsOptions.setValue(values);
+
+        extractorsOptions.addThemeVariants(CheckboxGroupVariant.LUMO_VERTICAL);
+        add(extractorsOptions);
     }
 }
