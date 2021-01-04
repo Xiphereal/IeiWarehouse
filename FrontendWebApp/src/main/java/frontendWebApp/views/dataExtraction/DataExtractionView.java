8af891e671cf6a9346aca3371a6bac540d6d8448
Xiphereal <lozanojalex@gmail.com>
2021-01-04 22:36:39 +0100

    Add basic layout for the data extraction view

--- FrontendWebApp/src/main/java/frontendWebApp/views/dataExtraction/DataExtractionView.java
+++ FrontendWebApp/src/main/java/frontendWebApp/views/dataExtraction/DataExtractionView.java
@@ -4,8 +4,10 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.CheckboxGroup;
 import com.vaadin.flow.component.checkbox.CheckboxGroupVariant;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -18,39 +20,80 @@ import java.util.Set;
 @PageTitle("Extracción de datos")
 @CssImport("./styles/views/dataExtraction/dataextraction-view.css")
 public class DataExtractionView extends HorizontalLayout {
+    private TextField startYear;
+    private TextField endYear;
 
-    private TextField name;
-    private Button sayHello;
+    private boolean extractFromDblp;
+    private boolean extractFromIeee;
+    private boolean extractFromGoogleScholar;
 
     public DataExtractionView() {
         setId("dataextraction-view");
 
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-
-        add(name, sayHello);
-
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        addExtractorsCheckboxOptions();
-
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
+        addComponentsToView();
     }
 
-    private void addExtractorsCheckboxOptions() {
+    private void addComponentsToView() {
+        H1 pageTitle = new H1("Extracción de referencias bibliográficas IEI");
+
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.add(pageTitle);
+
+        addExtractorsCheckboxOptionsTo(verticalLayout);
+
+        startYear = new TextField("Desde año");
+        endYear = new TextField("Hasta año");
+
+        HorizontalLayout yearRangeLayout = new HorizontalLayout();
+        yearRangeLayout.add(startYear, endYear);
+
+        Button loadButton = new Button("Cargar");
+        loadButton.addClickListener(e -> Notification.show("Extrayendo referencias bibliográficas a la Warehouse..."));
+
+        Button clearFiltersButton = new Button("Limpiar filtros");
+        clearFiltersButton.addClickListener(e -> clearAllFilters());
+
+        HorizontalLayout buttonsLayout = new HorizontalLayout();
+        buttonsLayout.add(loadButton, clearFiltersButton);
+
+        verticalLayout.add(yearRangeLayout, buttonsLayout);
+
+        verticalLayout.setAlignItems(Alignment.CENTER);
+        verticalLayout.setAlignSelf(Alignment.CENTER);
+
+        add(verticalLayout);
+    }
+
+    private void addExtractorsCheckboxOptionsTo(VerticalLayout verticalLayout) {
+        final String dblpOptionName = "DBLP";
+        final String ieeeOptionName = "IEEE";
+        final String googleScholarOptionName = "Google Scholar";
+
         CheckboxGroup<String> extractorsOptions = new CheckboxGroup<>();
         extractorsOptions.setLabel("Extractores a utilizar");
-        extractorsOptions.setItems("DBLP", "IEEE", "Google Scholar");
+        extractorsOptions.setItems(dblpOptionName, ieeeOptionName, googleScholarOptionName);
 
         Set<String> values = new HashSet<>();
-        values.add("DBLP");
-        values.add("IEEE");
-        values.add("Google Scholar");
+        values.add(dblpOptionName);
+        values.add(ieeeOptionName);
+        values.add(googleScholarOptionName);
         extractorsOptions.setValue(values);
 
         extractorsOptions.addThemeVariants(CheckboxGroupVariant.LUMO_VERTICAL);
-        add(extractorsOptions);
+
+        extractorsOptions.addSelectionListener(selection -> {
+            Set<String> selectedOptionsNames = selection.getValue();
+
+            extractFromDblp = selectedOptionsNames.contains(dblpOptionName);
+            extractFromIeee = selectedOptionsNames.contains(ieeeOptionName);
+            extractFromGoogleScholar = selectedOptionsNames.contains(googleScholarOptionName);
+        });
+
+        verticalLayout.add(extractorsOptions);
+    }
+
+    private void clearAllFilters() {
+        startYear.clear();
+        endYear.clear();
     }
 }
