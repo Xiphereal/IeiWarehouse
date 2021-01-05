1f94c06be9bc5f0e32cc30a183416caa3b9fb91d
Xiphereal <lozanojalex@gmail.com>
2021-01-05 15:21:08 +0100

    Rename module entrypoint

--- DblpWrapper/src/main/java/dblpWrapper/DblpWrapper.java
+++ DblpWrapper/src/main/java/dblpWrapper/DblpWrapper.java
@@ -1,21 +1,15 @@
 package dblpWrapper;
 
-import dblpWrapper.JSONtoXML.JsonToXmlConverter;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
-public class DBLPWrapper {
+public class DblpWrapper {
     public static void main(String[] args){
        //System.out.println(JsonToXmlConverter.convert().toString(3));
 
-       SpringApplication.run(DBLPWrapper.class, args);
+       SpringApplication.run(DblpWrapper.class, args);
     }
 }
