f770898d9c1382bdc69cbeccfd7441f13d6e7518
JV-DM <javidmwork@outlook.com>
2021-01-05 19:18:30 +0100

    added usefull comments

--- GoogleScholarWrapper/src/main/java/googleScholarWrapper/GoogleScholarWrapper.java
+++ GoogleScholarWrapper/src/main/java/googleScholarWrapper/GoogleScholarWrapper.java
@@ -10,18 +10,12 @@ import java.io.IOException;
 public class GoogleScholarWrapper {
 
     public static void main(String[] args) throws IOException {
-        // TODO: Replace the explicit call to scrap() here with the REST API
-        //  request and its associated parameters.
-        YearRange fakeYearRange = new YearRange();
-
+        //TODO:Add selenium scrapling and remove getting the objects from sample.bib
         //SeleniumScraper seleniumScraper = new SeleniumScraper();
 
         //List<String> citationsAsBibtex =
         //        seleniumScraper.retrieveCitationsAsBibtex(fakeYearRange, null);
         //String sample =
         SpringApplication.run(GoogleScholarWrapper.class, args);
-        // Terminates the program normally.
-        // It seems that Selenium Web Driver keeps the main execution thread running
-        // even when finishing normally.
     }
 }
--- GoogleScholarWrapper/src/main/java/googleScholarWrapper/restService/GoogleSchoolarExtractorApi.java
+++ GoogleScholarWrapper/src/main/java/googleScholarWrapper/restService/GoogleSchoolarExtractorApi.java
@@ -19,6 +19,7 @@ public class GoogleSchoolarExtractorApi {
     private static final String ERROR_MESSAGE = "ERROR: The given parameters for the search are invalid. " +
             "The years must use format startYear:yyyy and endYear:yyyy";
 
+    //TODO:Remove getting the string from file and add selenium scrapling
     @GetMapping("/extract")
     public Map<String, Object> getData(@RequestParam(value = "startYear", defaultValue = "1000") String startYear,
                                        @RequestParam(value = "endYear", defaultValue = "2999") String endYear) throws IOException {
@@ -35,6 +36,7 @@ public class GoogleSchoolarExtractorApi {
                 seleniumScraper.retrieveCitationsAsBibtex(yearRange, null);
         */
         String string = Files.readString(Path.of("D:\\IdeaProjects\\IeiWarehouse\\GoogleScholarWrapper\\src\\main\\resources\\sample.bib"));
+        //when using selenium we wont need to split the string since we will get a List<String>
         return BibtexToJsonParser.toJson(Arrays.asList(string.split("@"))).toMap();
     }
 }
