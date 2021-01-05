60f611774c77010ed8bf168b3c471c12e117538e
Xiphereal <lozanojalex@gmail.com>
2021-01-05 15:21:47 +0100

    Remove commented code

--- DblpWrapper/src/main/java/dblpWrapper/DblpWrapper.java
+++ DblpWrapper/src/main/java/dblpWrapper/DblpWrapper.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DblpWrapper {
     public static void main(String[] args){
-       //System.out.println(JsonToXmlConverter.convert().toString(3));
-
        SpringApplication.run(DblpWrapper.class, args);
     }
 }
