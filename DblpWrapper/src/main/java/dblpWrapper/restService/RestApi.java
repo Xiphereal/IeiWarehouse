1a4bd5f5228626b1c2381286c6591f65997c484b
Xiphereal <lozanojalex@gmail.com>
2021-01-05 20:36:10 +0100

    Remove unused fields

--- DblpWrapper/src/main/java/dblpWrapper/restService/RestApi.java
+++ DblpWrapper/src/main/java/dblpWrapper/restService/RestApi.java
@@ -10,19 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class RestApi {
-    private final AtomicLong requestId = new AtomicLong();
-
-    private static final String ERROR_MESSAGE = "ERROR: The given years for the extraction are invalid." +
-            "The expected parameters are 'startedYear:yyyy' and 'endYear:yyyy'" +
-            ", with startedYear being less than endYear." +
-            "The valid year range goes from 1000 to 2999";
-
-    private static final String OK_MESSAGE = "OK: The extraction has started as expected.";
-
     @GetMapping("/extract")
     public RequestResponse extractData(@RequestParam(value = "startYear", defaultValue = "1000") String startYear,
                                        @RequestParam(value = "endYear", defaultValue = "2999") String endYear) throws IOException {
