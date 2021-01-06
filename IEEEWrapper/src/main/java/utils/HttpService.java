64e8b06a8cdaa62297b9082559883bc08de2f5ff
RICARDO SOLER BACHS <risobac@upv.edu.es>
2021-01-06 21:20:35 +0100

    Add class HttpService

---
+++ IEEEWrapper/src/main/java/utils/HttpService.java
@@ -0,0 +1,29 @@
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+public class HttpService {
+    public static String executeGet(final String httpUrl) {
+        String response = "";
+
+        try {
+            URL url = new URL(httpUrl);
+
+            URLConnection urlConnection = url.openConnection();
+
+            InputStream inputStream = urlConnection.getInputStream();
+
+            Scanner s = new Scanner(inputStream).useDelimiter("\\A");
+            response = s.hasNext() ? s.next() : "";
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+}
