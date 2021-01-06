635d77fd404c2c586ab690416d4b1827c6001389
RICARDO SOLER BACHS <risobac@upv.edu.es>
2021-01-06 22:18:40 +0100

    Reorganize project

--- IEEEWrapper/src/main/java/IEEEWrapper/restService/requestResponses/RestApi.java
+++ IEEEWrapper/src/main/java/IEEEWrapper/restService/requestResponses/RestApi.java
@@ -1,4 +1,4 @@
-package restService;
+package IEEEWrapper.restService.requestResponses;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
