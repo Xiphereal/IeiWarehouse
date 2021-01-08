346ac4c6c9c6a0cd82f3083eaf4bf39db279148b
RICARDO SOLER BACHS <risobac@upv.edu.es>
2021-01-08 16:48:03 +0100

    Make URL as a global string

--- IEEEWrapper/src/main/java/IEEEWrapper/restService/requestResponses/RestApi.java
+++ IEEEWrapper/src/main/java/IEEEWrapper/restService/requestResponses/RestApi.java
@@ -7,6 +7,9 @@ import utils.HttpService;
 
 @RestController
 public class RestApi {
+
+    private final String ieeeURl = "http://ieeexploreapi.ieee.org/api/v1/search/articles?apikey=5sgmwkb7768ztxdw2vcze5x3&format=json";
+
     @GetMapping("/extract")
     public String extractData(@RequestParam(value = "startYear", defaultValue = "1000") String startYear,
                               @RequestParam(value = "endYear", defaultValue = "2999") String endYear,
@@ -15,8 +18,7 @@ public class RestApi {
     }
 
     private String getDataFromIEEE(int startYear, int endYear, int maxArticles) {
-        String ieeeQuery = "http://ieeexploreapi.ieee.org/api/v1/search/articles?apikey=5sgmwkb7768ztxdw2vcze5x3&format=json&max_records=" +
-                maxArticles + "&start_record=1&sort_order=asc&sort_field=article_number&start_year=" + startYear +
+        String ieeeQuery = ieeeURl + "&max_records=" + maxArticles + "&start_record=1&sort_order=asc&sort_field=article_number&start_year=" + startYear +
                 "&end_year=" + endYear;
 
         String jsonRetrieved = HttpService.executeGet(ieeeQuery);
