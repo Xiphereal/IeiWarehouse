6a4a70095ed598b57e9354b1ded7c7919ab4995a
JV-DM <javidmwork@outlook.com>
2021-01-08 11:41:56 +0100

    uncomment

--- Warehouse/src/main/java/warehouse/restService/HttpRequest.java
+++ Warehouse/src/main/java/warehouse/restService/HttpRequest.java
@@ -11,13 +11,14 @@ public class HttpRequest {
             .version(HttpClient.Version.HTTP_1_1)
             .connectTimeout(Duration.ofSeconds(10))
             .build();
+
     public static String GET(String url) throws IOException, InterruptedException {
         java.net.http.HttpRequest request = java.net.http.HttpRequest.newBuilder()
                 .GET().uri(URI.create(url))
                 .setHeader("User-Agent", "Java 11 HttpClient Bot")
                 .build();
 
-        HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
         return response.body();
     }
