2dbd454dba00da5e65060182b4231e185982da36
Xiphereal <lozanojalex@gmail.com>
2021-01-08 21:25:27 +0100

    Optimize imports

--- GoogleScholarWrapper/src/main/java/googleScholarWrapper/GoogleScholarWrapper.java
+++ GoogleScholarWrapper/src/main/java/googleScholarWrapper/GoogleScholarWrapper.java
@@ -1,10 +1,7 @@
 package googleScholarWrapper;
 
-import domainModel.utils.YearRange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class GoogleScholarWrapper {
