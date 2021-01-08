87bb8b8aa29eb46b5b80f73c7bc3c926487d8442
Xiphereal <lozanojalex@gmail.com>
2021-01-08 23:06:36 +0100

    Optimized imports in SeleniumScraper

--- GoogleScholarWrapper/src/main/java/googleScholarWrapper/selenium/SeleniumScraper.java
+++ GoogleScholarWrapper/src/main/java/googleScholarWrapper/selenium/SeleniumScraper.java
@@ -15,7 +15,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
