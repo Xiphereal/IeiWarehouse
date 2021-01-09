ff405b6c5e066890a945fe081f5d7333330b8ccd
Xiphereal <lozanojalex@gmail.com>
2021-01-09 15:27:59 +0100

    Minor renaming in RestApi

--- IeeeWrapper/src/main/java/ieeeWrapper/restService/RestApi.java
+++ IeeeWrapper/src/main/java/ieeeWrapper/restService/RestApi.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RestApi {
 
-    private final String ieeeURl = "http://ieeexploreapi.ieee.org/api/v1/search/articles?apikey=5sgmwkb7768ztxdw2vcze5x3&format=json";
+    private final String IEEE_BASE_URL_REQUEST =
+            "http://ieeexploreapi.ieee.org/api/v1/search/articles?apikey=5sgmwkb7768ztxdw2vcze5x3&format=json";
 
     @GetMapping("/extract")
     public String extractData(@RequestParam(value = "startYear", defaultValue = "1000") String startYear,
@@ -18,11 +19,12 @@ public class RestApi {
     }
 
     private String getDataFromIEEE(int startYear, int endYear, int maxArticles) {
-        String ieeeQuery = ieeeURl + "&max_records=" + maxArticles + "&start_record=1&sort_order=asc&sort_field=article_number&start_year=" + startYear +
+        String ieeeQuery = IEEE_BASE_URL_REQUEST + "&max_records=" + maxArticles +
+                "&start_record=1&sort_order=asc&sort_field=article_number&start_year=" + startYear +
                 "&end_year=" + endYear;
 
-        String jsonRetrieved = HttpService.executeGet(ieeeQuery);
+        String retrievedJsonResponse = HttpService.executeGet(ieeeQuery);
 
-        return jsonRetrieved;
+        return retrievedJsonResponse;
     }
 }
