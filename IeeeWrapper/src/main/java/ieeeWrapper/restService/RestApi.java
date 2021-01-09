ef8e35c626dab938c9b8d71aa69938ba3db19d14
Xiphereal <lozanojalex@gmail.com>
2021-01-09 15:28:29 +0100

    Remove redundant API parameter casting

--- IeeeWrapper/src/main/java/ieeeWrapper/restService/RestApi.java
+++ IeeeWrapper/src/main/java/ieeeWrapper/restService/RestApi.java
@@ -12,10 +12,10 @@ public class RestApi {
             "http://ieeexploreapi.ieee.org/api/v1/search/articles?apikey=5sgmwkb7768ztxdw2vcze5x3&format=json";
 
     @GetMapping("/extract")
-    public String extractData(@RequestParam(value = "startYear", defaultValue = "1000") String startYear,
-                              @RequestParam(value = "endYear", defaultValue = "2999") String endYear,
-                              @RequestParam(value = "maxPublications", defaultValue = "5") String maxPublications) {
-        return getDataFromIEEE(Integer.parseInt(startYear), Integer.parseInt(endYear), Integer.parseInt(maxPublications));
+    public String extractData(@RequestParam(value = "startYear", defaultValue = "1000") int startYear,
+                              @RequestParam(value = "endYear", defaultValue = "2999") int endYear,
+                              @RequestParam(value = "maxPublications", defaultValue = "5") int maxPublications) {
+        return getDataFromIEEE(startYear, endYear, maxPublications);
     }
 
     private String getDataFromIEEE(int startYear, int endYear, int maxArticles) {
